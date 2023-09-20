d21e934593d2ef78329a89c9d44e6b4db0eaf7a1
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-09-21 01:56:22 +0200

    Implement service without using `AOP`

--- src/main/java/com/aop/demo/DemoApplication.java
+++ src/main/java/com/aop/demo/DemoApplication.java
@@ -1,15 +1,23 @@
 package com.aop.demo;
 
+import com.aop.demo.entities.Comment;
+import com.aop.demo.services.CommentsService;
+import com.aop.demo.services.impl.CommentsServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 	private final static Logger LOGGER =
 			LoggerFactory.getLogger(DemoApplication.class);
+
+	@Autowired
+	private CommentsService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -17,6 +25,12 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		LOGGER.info("Starting the application");
+
+		var comment = new Comment("Comment 1 content","Author 1");
+
+		service.publishComment(comment);
+
 	}
 }
---
+++ src/main/java/com/aop/demo/services/impl/CommentsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.aop.demo.services.impl;
+
+import com.aop.demo.entities.Comment;
+import com.aop.demo.services.CommentsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentsServiceImpl implements CommentsService {
+
+    private final static Logger LOGGER =
+            LoggerFactory.getLogger(CommentsServiceImpl.class);
+
+    @Override
+    public void publishComment(Comment comment) {
+        LOGGER.info("Publishing comment '" + comment.content() + "' inside the target method.");
+    }
+}
